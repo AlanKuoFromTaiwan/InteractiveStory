c72adfb486e3bee57b9faac55a5192779d6f69e7
Alan <gn26422458@yahoo.com.tw>
2018-09-03 09:52:20 +0800

    Set up the story page and the context

---
+++ app/src/main/java/com/alan/interactivestory/model/Choice.java
@@ -0,0 +1,27 @@
+package com.alan.interactivestory.model;
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
